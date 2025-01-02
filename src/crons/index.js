4a26b494ab40bd1c1a2e08de8d91adad8e012e9f
Erric Rapsing <erric.rapsing@gmail.com>
2025-01-02 22:23:28 +0800

    REFACTOR: cron definitions to export only the schedule and job to execute

--- src/crons/index.js
+++ src/crons/index.js
@@ -1,1 +1,49 @@
+const fs = require("fs");
+const cron = require("node-cron");
+const { logger } = require("../helpers/logging");
 require("./stale-user-access");
+
+/**
+ * @typedef {Object} CronJob
+ * @property {string} filename
+ * @property {cron.ScheduledTask} job cron schedule
+ */
+
+/**
+ * @type {CronJob[]} list of cron jobs
+ */
+const crons = [];
+
+logger.info(`Scanning for cron jobs..`);
+for (let file of fs.readdirSync(__dirname)) {
+  if (file === "index.js" || !file.endsWith(".js")) {
+    continue;
+  }
+
+  const item = require(`./${file}`);
+
+  // validate module require
+  if (!cron.validate(item.schedule)) {
+    logger.warn(`Invalid cron schedule file: ${file}`);
+  } else if (typeof item.job !== "function") {
+    logger.warn(`Invalid cron job function file: ${file}`);
+  } else {
+    logger.info(`Started cron: ${file}, schedule: ${item.schedule}`);
+    crons.push({
+      filename: file,
+      job: cron.schedule(item.schedule, () =>
+        item.job().catch((err) => logger.error(err))
+      ),
+    });
+  }
+}
+
+const shutdown = (signal) => async () => {
+  logger.warn("Stopping cron schedules.");
+  for (let item of crons) {
+    item.job.stop();
+  }
+};
+
+process.on("SIGINT", shutdown("SIGINT"));
+process.on("SIGTERM", shutdown("SIGINT"));
--- src/crons/stale-user-access.js
+++ src/crons/stale-user-access.js
@@ -1,16 +1,8 @@
 const { logger } = require("../helpers/logging");
 const { CRON_CHECK_STALE_USER_ACCESS } = require("../startup/environment");
-const cron = require("node-cron");
 
-logger.info(`starting cron: stale-user-access ${CRON_CHECK_STALE_USER_ACCESS}`);
-const schedule = cron.schedule(CRON_CHECK_STALE_USER_ACCESS, async () => {
+exports.schedule = CRON_CHECK_STALE_USER_ACCESS;
+
+exports.job = async () => {
   logger.info("check stale user access");
-});
-
-const shutdown = (signal) => () => {
-  logger.warn("stopping cron: stale-user-access");
-  schedule.stop();
 };
-
-process.on("SIGINT", shutdown("SIGINT"));
-process.on("SIGTERM", shutdown("SIGINT"));
